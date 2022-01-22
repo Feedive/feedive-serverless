e46d0b903c2a29e9eb995285a308fa5322d6666f
谢沛东 <peidongxie@outlook.com>
2022-01-22 20:37:57 +0800

    chore: 实现工程化配置

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+const typescript = require('@rollup/plugin-typescript');
+
+export default {
+  external: ['koa'],
+  input: './src/index.ts',
+  output: {
+    file: './build/index.js',
+    format: 'cjs',
+    exports: 'auto',
+  },
+  plugins: [typescript({ composite: false })],
+};
