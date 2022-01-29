5dbb0107bc3b7f96a6b9d0bd34049509975fdf24
谢沛东 <peidongxie@outlook.com>
2022-01-29 16:27:27 +0800

    feat: 优化路由

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import allHello from './route/hello/all';
 import getRssJavaScriptWeekly from './route/rss/javascript-weekly/get';
 
 const app = new Server();
-app.route('ALL', /^\/hello$/, allHello);
+app.route('ALL', '/hello', allHello);
 app.route('GET', '/rss/javascript-weekly', getRssJavaScriptWeekly);
 
 export default app;
