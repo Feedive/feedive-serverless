1fbaa4a8fcb97d30b893d87af53650858ed3854d
谢沛东 <peidongxie@outlook.com>
2022-02-08 16:26:15 +0800

    feat: 实现微博用户信息查询接口

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import { Server } from './wrap-http';
 import allHello from './route-service/hello/all';
 import getRssJavaScriptWeekly from './route-service/rss/javascript-weekly/get';
 import getRssWechat from './route-service/rss/wechat/get';
+import getRssWeibo from './route-service/rss/weibo/get';
 
 const app = new Server();
 app.route('ALL', '/hello', allHello);
 app.route('GET', '/rss/javascript-weekly', getRssJavaScriptWeekly);
 app.route('GET', '/rss/wechat', getRssWechat);
+app.route('GET', '/rss/weibo', getRssWeibo);
 
 export { app as default };
