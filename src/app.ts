a6dda430644b6d0a76cced9680452332fb7c9f2c
谢沛东 <peidongxie@outlook.com>
2022-02-22 17:18:32 +0800

    feat: 实现b站动态查询接口

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { Server } from './wrap-http';
 import allHello from './route-service/hello/all';
+import getRssBilibili from './route-service/rss/bilibili/get';
 import getRssJavaScriptWeekly from './route-service/rss/javascript-weekly/get';
 import getRssWechat from './route-service/rss/wechat/get';
 import getRssWeibo from './route-service/rss/weibo/get';
 
 const app = new Server();
 app.route('ALL', '/hello', allHello);
+app.route('GET', '/rss/bilibili', getRssBilibili);
 app.route('GET', '/rss/javascript-weekly', getRssJavaScriptWeekly);
 app.route('GET', '/rss/wechat', getRssWechat);
 app.route('GET', '/rss/weibo', getRssWeibo);
