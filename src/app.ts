1cea38f2754f4b353aff7488b6c2b85b8251a16c
谢沛东 <peidongxie@outlook.com>
2022-03-08 19:04:53 +0800

    feat: 启用 Atom 和 JSON Feed

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import { Server } from './wrap-http';
+import getAtomBilibili from './route-service/atom/bilibili/get';
+import getAtomWechat from './route-service/atom/wechat/get';
+import getAtomWeibo from './route-service/atom/weibo/get';
 import allHello from './route-service/hello/all';
+import getJsonBilibili from './route-service/json/bilibili/get';
+import getJsonWechat from './route-service/json/wechat/get';
+import getJsonWeibo from './route-service/json/weibo/get';
 import getRssBilibili from './route-service/rss/bilibili/get';
 import getRssJavaScriptWeekly from './route-service/rss/javascript-weekly/get';
 import getRssWechat from './route-service/rss/wechat/get';
 import getRssWeibo from './route-service/rss/weibo/get';
 
 const app = new Server();
+app.route('GET', '/atom/bilibili', getAtomBilibili);
+app.route('GET', '/atom/wechat', getAtomWechat);
+app.route('GET', '/atom/weibo', getAtomWeibo);
 app.route('ALL', '/hello', allHello);
+app.route('GET', '/json/bilibili', getJsonBilibili);
+app.route('GET', '/json/wechat', getJsonWechat);
+app.route('GET', '/json/weibo', getJsonWeibo);
 app.route('GET', '/rss/bilibili', getRssBilibili);
 app.route('GET', '/rss/javascript-weekly', getRssJavaScriptWeekly);
 app.route('GET', '/rss/wechat', getRssWechat);
---
+++ src/route-service/json/bilibili/get.ts
@@ -0,0 +1,27 @@
+import { Feed } from 'feed';
+import { type Handler } from '../../../wrap-http';
+import retrieveBilibiliListAndSummary from '../../../api-persistence/retrieve-bilibili-list-and-summary';
+import retrieveBilibiliItem from '../../../api-persistence/retrieve-bilibili-item';
+
+const handler: Handler = async (req) => {
+  const url = req.getUrl();
+  const id = url.searchParams.get('id');
+  if (id === null) return { code: 400 };
+  const { summary: feedOptions, list } = await retrieveBilibiliListAndSummary(
+    id,
+  );
+  const feed = new Feed(feedOptions);
+  for (const item of await Promise.all(
+    list.map(({ item, itemParams }) => retrieveBilibiliItem(item, itemParams)),
+  )) {
+    feed.addItem(item);
+  }
+  return {
+    headers: {
+      ['Content-Type']: 'text/xml; charset=utf-8',
+    },
+    body: feed.json1(),
+  };
+};
+
+export { handler as default };
