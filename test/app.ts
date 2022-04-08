2d628a85bc47d9cc579f8fe730974cbddfed58cd
谢沛东 <peidongxie@outlook.com>
2022-04-08 16:49:20 +0800

    test: 测试 web server

--- test/app.ts
+++ test/app.ts
@@ -1,6 +1,6 @@
 import app from '../src/app';
 import { Server } from '../src/wrap-http';
 
-test('app is an instance of Server', () => {
+test('app is an instance of server', () => {
   expect(app).toBeInstanceOf(Server);
 });
